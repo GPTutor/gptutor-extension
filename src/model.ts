1746cf513e62f36962e932bad57dad554f24e9e4
EasonC13 <gtes9950406@gmail.com>
2023-06-14 09:52:52 +0800

    refactor: remove unnecessary code in model.ts

--- src/model.ts
+++ src/model.ts
@@ -1,27 +1,5 @@
 import { ExtensionContext, window, workspace } from "vscode";
 import * as vscode from "vscode";
-export async function getModel(context: ExtensionContext) {
-  const options: {
-    [key: string]: (context: ExtensionContext) => Promise<void>;
-  } = {
-    "gpt-3.5-turbo (default)": async () => {
-      setModel("gpt-3.5-turbo");
-    },
-    "gpt-4": async () => {
-      setModel("gpt-4");
-    },
-  };
-  const quickPick = window.createQuickPick();
-  quickPick.items = Object.keys(options).map((label) => ({ label }));
-  quickPick.onDidChangeSelection((selection) => {
-    if (selection[0]) {
-      options[selection[0].label](context).catch(console.error);
-    }
-    quickPick.hide();
-  });
-  quickPick.onDidHide(() => quickPick.dispose());
-  quickPick.show();
-}
 
 let models = ["gpt-3.5-turbo", "gpt-4"];
 
