b2a89172b12db35e8da2d874d95ec4c283d9b6a2
EasonC13 <eason.tw.chen@gmail.com>
2023-12-11 07:52:23 +0800

    add inlay to text success

--- src/getCurrentPromptV2.ts
+++ src/getCurrentPromptV2.ts
@@ -17,8 +17,11 @@ export async function getCurrentPromptV2(
 
   const document = editor.document;
   const languageId = document.languageId;
+  // Check if the languageId is "move"
   if (languageId == "move") {
+    // Import the move_analyzer_context module
     const move_analyzer_context_1 = await import("./move_analyzer_context.js");
+    // Define the configuration object for move_analyzer_context
     const configuration = {
       server: { path: "/Users/eason/.cargo/bin/move-analyzer" },
       serverPath: "/Users/eason/.cargo/bin/move-analyzer",
@@ -31,16 +34,21 @@ export async function getCurrentPromptV2(
       },
       trace: { server: "off" },
     };
+    // Create an instance of move_analyzer_context with the provided extensionContext and configuration
     const move_analyzer_context: any = move_analyzer_context_1.Context.create(
       extensionContext,
       configuration
     );
+    // Start the client for move_analyzer_context
     await move_analyzer_context.startClient();
     const client = move_analyzer_context.getClient();
 
+    // If the client is undefined, return undefined
     if (client === undefined) {
       return undefined;
     }
+
+    // Send a request for inlay hints using the client
     const hints = await client.sendRequest("textDocument/inlayHint", {
       range: [
         { line: 0, character: 0 },
@@ -48,7 +56,44 @@ export async function getCurrentPromptV2(
       ],
       textDocument: { uri: document.uri.toString() },
     });
+
+    // Log the line count and the first hint
+    console.log(document.lineCount, hints[0]);
+
+    const text = document.getText();
+    let lines = text.split("\n");
     console.log(hints);
+    let linesPrePadding = Object();
+
+    // Iterate through the hints
+    for (let i = 0; i < hints.length; i++) {
+      const hint = hints[i];
+      if (hint.kind === 2) {
+        console.log(lines[hint.position.line]);
+        let line = lines[hint.position.line];
+        if (hint.position.character <= line.length) {
+          // Insert the hint value at the specified position in the line
+          if (!linesPrePadding[hint.position.line]) {
+            linesPrePadding[hint.position.line] = 0;
+          }
+
+          const newLine =
+            line.substring(
+              0,
+              hint.position.character + linesPrePadding[hint.position.line]
+            ) +
+            hint.label[0].value +
+            line.substring(
+              hint.position.character + linesPrePadding[hint.position.line]
+            );
+          lines[hint.position.line] = newLine;
+          linesPrePadding[hint.position.line] += hint.label[0].value.length;
+        } else {
+          console.error("Invalid character position");
+        }
+      }
+    }
+    console.log(lines.join("\n"));
   }
 
   const currentTextLines = document.getText().split("\n");
